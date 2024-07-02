e67e4bfd07ef50d004f84910428ff69cc93c43bf
Mert Karadayi <imertkaradayi@gmail.com>
2024-07-03 01:31:09 +0300

    navbar menu icon, responsive design for mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
     <nav>
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
+          <span>LamaEstate</span>
         </a>
 
         <a href="/">Home</a>
@@ -18,6 +21,22 @@ function Navbar() {
         <a href="/" className="register">
           Sign up
         </a>
+        <div class="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+
+        <div class={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
